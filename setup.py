de8a5899f95073d88c322b8b4adf1311f038c667
Anshuman Aich <anshuman.aich@bacancy.com>
2024-10-15 22:20:21 +0530

    added setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='5x5-leadfuze-matched',
+    use_scm_version=True,  # Enables versioning via setuptools_scm
+    setup_requires=['setuptools_scm'],
+)
+
